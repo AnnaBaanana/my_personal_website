4f4830febdbb762f5e521f3ec7a5fbff99b34141
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-03-22 23:17:03 -0400

    adding chnages to css, html, javascript

--- script.js
+++ script.js
@@ -2,3 +2,31 @@ const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
+
+function daysProgramming(x,y) {
+    var start_date = new Date("2/27/2022");
+    var date_now = new Date();
+    const diffTime = Math.abs(date_now - start_date);
+    const diffDays =diffTime/(1000*60*60*24)
+    return `I have been coding for ${diffDays} days now.`;
+    
+}
+
+daysProgramming();
+
+document.getElementById("program").innerHTML =  daysProgramming();
+
+function zoomIn(event) {
+    var element = document.getElementById("overlay");
+    element.style.display = "inline-block";
+    var img = document.getElementById("imgZoom");
+    var posX = event.offsetX ? (event.offsetX) : event.pageX - img.offsetLeft;
+    var posY = event.offsetY ? (event.offsetY) : event.pageY - img.offsetTop;
+    element.style.backgroundPosition=(-posX*2)+"px "+(-posY*4)+"px";
+  
+  }
+  
+  function zoomOut() {
+    var element = document.getElementById("overlay");
+    element.style.display = "none";
+  }
